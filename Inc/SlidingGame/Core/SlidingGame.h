0c46862f311ff09b3b7138f3cc3206296502b323
codin <codingpupper3033@gmail.com>
2026-01-28 23:21:02 -0500

    Add documentation for SlidingGame class methods and constructors

--- Inc/SlidingGame/Core/SlidingGame.h
+++ Inc/SlidingGame/Core/SlidingGame.h
@@ -22,7 +22,16 @@ namespace SlidingGame::Core
             RIGHT
         };
 
+        /**
+         * Sets up a sliding game with given size (width and height are equal)
+         * @param size size of the board (width and height)
+         */
         explicit SlidingGame(const Index size): SlidingGame(size, size) {}
+        /**
+         * Sets up a sliding game with given width and height
+         * @param width width of the board
+         * @param height height of the board
+         */
         SlidingGame(Index width, Index height);
 
         /* Getters */
@@ -30,9 +39,29 @@ namespace SlidingGame::Core
         [[nodiscard]] auto getHeight() const -> Index { return board.getHeight(); }
         [[nodiscard]] auto getMoves() const -> Index { return moves; }
 
+        /**
+         * Gets the cell at the given position.
+         * @param x The x coordinate.
+         * @param y The y coordinate.
+         * @return A reference to the cell at the given position.
+         */
         [[nodiscard]] auto at(Coord x, Coord y) const -> const Cell&;
+
+        /**
+         * Gets a row of cells at the given y coordinate.
+         * @param y The y coordinate.
+         * @return A vector of pointers to the cells in the row.
+         */
         [[nodiscard]] auto getRow(Coord y) const -> std::vector<const Cell*>;
 
+        /**
+         * Offsets the given coordinates in the specified direction by the given offset.
+         * @param x The x coordinate.
+         * @param y The y coordinate.
+         * @param direction The direction to offset.
+         * @param offset The amount to offset.
+         * @return A pair of the new x and y coordinates.
+         */
         [[nodiscard]] static auto offsetCoord(Coord x, Coord y, Direction direction, Coord offset) -> std::pair<Coord, Coord>;
 
         /* Methods */
